4c383be8f6bac319c22e4d613b7e4761aa6c7fbb
james gill <james@jgdev.me>
2018-10-27 12:53:22 -0700

    added bouncing arrows

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -1,36 +1,60 @@
-$(document).ready(function() {
+$(document).ready(function () {
   data.forEach((portfolioItem, i) => {
     const tags = portfolioItem.tags.map((tag, i) => {
       return (
-      $('<div>', { class: 'tag-container'}).append(
-        $('<span>', { class: 'tag', text: tag })
-      ));
+        $('<div>', {
+          class: 'tag-container'
+        }).append(
+          $('<span>', {
+            class: 'tag',
+            text: tag
+          })
+        ));
     });
 
-    const portfolio = $('<li>', { class: 'portfolio__item'}).append(
-      $('<a>', { class: 'screen', target: '_origin', href: portfolioItem.url }).append(
-        $('<div>', { class: 'bar' }).append(
-          $('<h2>', { text: portfolioItem.title })
+    const portfolio = $('<li>', {
+      class: 'portfolio__item'
+    }).append(
+      $('<a>', {
+        class: 'screen',
+        target: '_origin',
+        href: portfolioItem.url
+      }).append(
+        $('<div>', {
+          class: 'bar'
+        }).append(
+          $('<h2>', {
+            text: portfolioItem.title
+          })
         ).append(
           $('<i>')
         )
       ).append(
-        $('<div>', { class: 'main' }).append(
-          $('<img>', { src: portfolioItem.img })
+        $('<div>', {
+          class: 'main'
+        }).append(
+          $('<img>', {
+            src: portfolioItem.img
+          })
         )
       ).append(
-        $('<a>', { class: 'screen overlay', target: '_origin', href: portfolioItem.url }).append(
-          $('<h3>', { text: 'Technologies Used'})
+        $('<a>', {
+          class: 'screen overlay',
+          target: '_origin',
+          href: portfolioItem.url
+        }).append(
+          $('<h3>', {
+            text: 'Technologies Used'
+          })
         ).append(tags)
       )
     )
 
     $("#portfolio-list").append(portfolio);
- });
+  });
 });
 
-const data = [
-  {
+const data = [{
     title: 'Pomlist',
     url: 'http://pomlist.s3-website-us-west-2.amazonaws.com/',
     img: 'https://image.flaticon.com/icons/svg/135/135702.svg',
@@ -43,16 +67,16 @@ const data = [
     tags: ['React', 'Redux', 'Node', 'Cron', 'Passport.js', 'REST']
   },
   {
-    title: 'Project Ada',
-    url: 'https://www.projectada.ca',
-    img: 'https://image.flaticon.com/icons/svg/1111/1111933.svg',
-    tags: ['Wordpress', 'Gulp', 'Javascript', 'Jquery', 'HTML5', 'CSS3', 'SASS']
-  },
-  {
     title: 'Life Happens',
     url: 'http://goo.gl/BSrauj',
     img: 'https://image.flaticon.com/icons/svg/993/993723.svg',
     tags: ['React Native', 'EXPO', 'OAuth']
+  },
+  {
+    title: 'Project Ada',
+    url: 'http://www.projectada.ca',
+    img: 'https://image.flaticon.com/icons/svg/1111/1111933.svg',
+    tags: ['Wordpress', 'Gulp', 'Javascript', 'Jquery', 'HTML5', 'CSS3', 'SASS']
   },
   {
     title: 'Canvas Homes',
@@ -62,8 +86,8 @@ const data = [
   },
   {
     title: 'Lead / Impact',
-    url: 'https://www.leadimpact.ca',
-    img: 'https://image.flaticon.com/icons/svg/201/201553.svg',
+    url: 'http://leadimpact.nicolawealth.com',
+    img: 'http://image.flaticon.com/icons/svg/201/201553.svg',
     tags: ['Wordpress', 'Gulp', 'Javascript', 'Jquery', 'HTML5', 'CSS3', 'SASS']
   }
 ];
--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,39 +1,48 @@
-$(document).ready(function() {
-    $('.light-switch').click((e) => {
-      $('body').toggleClass("body--white body--colored");
+$(document).ready(function () {
+  $('.light-switch').click((e) => {
+    $('body').toggleClass("body--white body--colored");
+  });
+
+  $('.toggle').on('click', function () {
+    if ($(this).hasClass('on')) {
+      $(this).removeClass('on');
+    } else {
+      $(this).addClass('on');
+    }
+  });
+
+  $(window).on("resize scroll", function () {
+    $('.section__title--about').waypoint(function (direction) {
+      $('.section__title--about svg').css('display', 'block');
+    }, {
+      offset: '75%'
     });
 
-    $('.toggle').on('click', function() {
-      if ($(this).hasClass('on')) {
-         $(this).removeClass('on');
-      } else {
-         $(this).addClass('on');
-      }
+    $('.section__title--portfolio').waypoint(function (direction) {
+      $('.section__title--portfolio svg').css('display', 'block');
+    }, {
+      offset: '75%'
     });
+  });
 
-    $(window).on("resize scroll", function() {
-      $('.section__title--about').waypoint(function(direction) {
-        $('.section__title--about svg').css('display', 'block');
-      }, {
-        offset: '75%'
-      });
+  $("nav a").click(function (e) {
+    e.preventDefault();
+    var dest = $(this).attr('href');
+    $('html,body').animate({
+      scrollTop: $(dest).offset().top
+    }, 'slow');
+  });
 
-      $('.section__title--portfolio').waypoint(function(direction) {
-        $('.section__title--portfolio svg').css('display', 'block');
-      }, {
-        offset: '75%'
-      });
-    });
-
-    $("nav a").click(function(e) {
-      console.log(e);
-      e.preventDefault();
-      var dest = $(this).attr('href');
-      $('html,body').animate({ scrollTop: $(dest).offset().top }, 'slow');
+  $(".page-anchor").click(function (e) {
+    e.preventDefault();
+    var dest = $(this).attr('href');
+    $('html,body').animate({
+      scrollTop: $(dest).offset().top
+    }, 'slow');
   });
 });
 
-$.fn.isInViewport = function() {
+$.fn.isInViewport = function () {
   var elementTop = $(this).offset().top;
   var elementBottom = elementTop + $(this).outerHeight();
   var viewportTop = $(window).scrollTop();
